3b2f4b79e3134734a5aab031d4256454e6e7b267
Ghostovich <gusarov_konstant@mail.ru>
2022-10-13 08:31:42 +0300

    3

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] GetArray (string a, string b, string c, string d)
+{
+    
+}
+
+
+
+string a = Console.ReadLine();
+string b = Console.ReadLine();
+string c = Console.ReadLine();
+string d = Console.ReadLine();
+
+int[] print = GetArray(a, b, c, d);
+Console.WriteLine($"->{print}");
